c1266d4a9d450e878af0cb29596622e005c3c953
NeoArmageddon <neoarmageddon@web.de>
2014-07-30 20:29:42 +0200

    Added commitsubfolder to jenkins

--- buildpbo.bat
+++ buildpbo.bat
@@ -1,24 +1,19 @@
 @echo off
 echo Starting build...
-::forfiles /p %WORKSPACE%\..\%JOB_NAME%_binned /c "cmd /c del @PATH /f /q"
-::rmdir /s /q %WORKSPACE%\..\packing\escape2.altis
-::cho Deleted old binned files
-::xcopy %WORKSPACE% %WORKSPACE%\..\packing\escape2.altis /e /i /q
 
 echo BUILD NR: %BUILD_NUMBER% > %WORKSPACE%\version.txt
 echo COMMIT: %GIT_COMMIT% >> %WORKSPACE%\version.txt
 echo BRANCH: %GIT_BRANCH% >> %WORKSPACE%\version.txt
 echo BUILD INFO: %BUILD_URL% >> %WORKSPACE%\version.txt
+echo  AUTHOR: %GIT_AUTHOR_EMAIL% >> %WORKSPACE%\version.txt
+echo  COMMITTER: %GIT_COMMITTER_EMAIL%  >> %WORKSPACE%\version.txt
 
 del %WORKSPACE%\co10_escape.altis\include\build.hpp /f /q
-echo #define BUILD %BUILD_NUMBER% >> %WORKSPACE%\checkout\escape.altis\include\build.hpp
-
+echo #define BUILD %BUILD_NUMBER% >> %WORKSPACE%\checkout\co10_escape.altis\include\build.hpp
+echo  #define COMMITTER %GIT_COMMITTER_EMAIL% >> %WORKSPACE%\checkout\co10_escape.altis\include\build.hpp
 echo Copied over folder to binned folder
-::"C:\Steam\SteamApps\common\Arma 3 Tools\FileBank\FileBank.exe" -dst "%WORKSPACE%" source "%WORKSPACE%\checkout\co10_escape.altis"
-"C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe" "%WORKSPACE%\co10_escape.altis" "%WORKSPACE%" -DEBUG -PREFIX ""
+"C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe" "%WORKSPACE%\checkout\co10_escape.altis" "%WORKSPACE%" -DEBUG -PREFIX ""
 echo Created PBOs
-xcopy "%WORKSPACE%\co10_escape.altis.pbo" "C:\Server\Apache2.4\htdocs\escape.anzp.de\escape_build%BUILD_NUMBER%.altis.pbo*"
+xcopy "%WORKSPACE%\co10_escape.altis.pbo" "C:\Server\Apache2.4\htdocs\escape.anzp.de\co10_escape_build%BUILD_NUMBER%.altis.pbo*"
 echo PBO copied to webserver
-::forfiles /p %WORKSPACE%\..\%JOB_NAME%_binned /c "cmd /c if @ISDIR==TRUE rmdir /s /q @PATH"
-::echo Removed binned folders
 if not exist %WORKSPACE%\co10_escape.altis.pbo exit /b 1
