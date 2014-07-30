5f8e541d91e549336c0c40d8b1d9872611c98e8a
NeoArmageddon <neoarmageddon@web.de>
2014-07-30 20:36:13 +0200

    Fixed a whitespace in buildscript

--- buildpbo.bat
+++ buildpbo.bat
@@ -5,12 +5,12 @@ echo BUILD NR: %BUILD_NUMBER% > %WORKSPACE%\version.txt
 echo COMMIT: %GIT_COMMIT% >> %WORKSPACE%\version.txt
 echo BRANCH: %GIT_BRANCH% >> %WORKSPACE%\version.txt
 echo BUILD INFO: %BUILD_URL% >> %WORKSPACE%\version.txt
-echo  AUTHOR: %GIT_AUTHOR_EMAIL% >> %WORKSPACE%\version.txt
-echo  COMMITTER: %GIT_COMMITTER_EMAIL%  >> %WORKSPACE%\version.txt
+echo AUTHOR: %GIT_AUTHOR_EMAIL% >> %WORKSPACE%\version.txt
+echo COMMITTER: %GIT_COMMITTER_EMAIL%  >> %WORKSPACE%\version.txt
 
 del %WORKSPACE%\co10_escape.altis\include\build.hpp /f /q
 echo #define BUILD %BUILD_NUMBER% >> %WORKSPACE%\checkout\co10_escape.altis\include\build.hpp
-echo  #define COMMITTER %GIT_COMMITTER_EMAIL% >> %WORKSPACE%\checkout\co10_escape.altis\include\build.hpp
+echo #define COMMITTER %GIT_COMMITTER_EMAIL% >> %WORKSPACE%\checkout\co10_escape.altis\include\build.hpp
 echo Copied over folder to binned folder
 "C:\Program Files (x86)\Bohemia Interactive\Tools\BinPBO Personal Edition\BinPBO.exe" "%WORKSPACE%\checkout\co10_escape.altis" "%WORKSPACE%" -DEBUG -PREFIX ""
 echo Created PBOs
